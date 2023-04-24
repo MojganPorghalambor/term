f5310bf257680f1ffa70edef3bf923e5f5bfdaad
MojganPorghalambor <moji.in.poul@gmail.com>
2023-04-24 15:43:16 +0330

    04/02/1402

--- term 3/t19.py
+++ term 3/t19.py
@@ -2,9 +2,10 @@
 from tkinter import *
 
 root=Tk()
-lf1=LabelFrame(root,text="Operator: ")
+lf1=LabelFrame(root,text="Operator: ",width=200,height=500)
 e1=Entry(lf1)
-lf1.pack()
+# lf1.grid(ipadx=100,ipady=200)
+lf1.place(x=50,y=70)
 e1.pack()
 
 
